b42bd6c2f46f6e389c217a3fb25969933af46b41
dgsquared <diego.dgomez@outlook.com>
2024-05-02 03:05:02 -0700

    okay i cant take it anymore

---
+++ four.js
@@ -0,0 +1,25 @@
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+
+  function draw() {
+    background(0);
+    fill('#FFFFFF');
+    //imageMode(CENTER);
+    //image(gif, width/2, height/2)
+    textAlign(CENTER);
+    textSize(50);
+    textFont('Adobe Garamond Pro');
+    text('every living creature years to gnaw our bones', width / 2, height * 4/6);
+    text('eyes glint in vengeance, urging us to suicide', width / 2, height * 5/6);
+    
+    push();
+        fill('#000000');
+        rect(0, height * 3.5/6, mouseX, height/8);
+        rect(0, height * 4.5/6, mouseX - height/7, height/8);
+    pop();
+  }
