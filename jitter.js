7cb7056492b909d2a0bf33c42d79afe302f27b36
dgsquared <diego.dgomez@outlook.com>
2024-05-01 23:50:19 -0700

    NOOOO

--- jitter.js
+++ jitter.js
@@ -1,21 +1,18 @@
-    let gif;
-
-    function windowResized() {
-      resizeCanvas(windowWidth, windowHeight);
-    }
     
-    function setup() {
-      createCanvas(windowWidth, windowHeight);
-      gif = loadImage('fourtwo.gif');
-    }
-    
-    function draw() {
-      background('#000000');
-        image(gif, 0, 0);
-      fill ('#FFFFFF');
-      textAlign(CENTER);
-      textFont('Adobe Garamond Pro');
-      textSize(40);
-      textFont('Adobe Garamond Pro');
-      text('god\'s creation rebuffs our assimilation', width / 2, height * 5/6);
-    }
+      function windowResized() {
+        resizeCanvas(windowWidth, windowHeight);
+      }
+      
+      function setup() {
+        createCanvas(windowWidth, windowHeight);
+        background(0, 0);
+        
+      }
+      
+      function draw() {
+        fill('#FFFFFF');
+        textAlign(CENTER);
+        textSize(30);
+        textFont('Adobe Garamond Pro');
+        text('our atmosphere refuses to enfold us', width / 2, height * 5/6);
+      }
