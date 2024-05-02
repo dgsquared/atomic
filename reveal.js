bc8d5e8d51b0f290a7b647da4222682e9e88c050
dgsquared <diego.dgomez@outlook.com>
2024-05-02 10:47:36 -0700

    waaa

--- reveal.js
+++ reveal.js
@@ -1,13 +1,27 @@
+let draft, ready;
+
+function preload() {
+  ready = loadImage('goatblur.jpg')
+  draft = loadImage('goatclear.jpg')
+}
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
   }
   
   function setup() {
     createCanvas(windowWidth, windowHeight);
+    image(ready, 0, 0);
+    image(draft, 0, 0);
+    
+  }
+
+  function mouseDragged() {
+    copy(ready, mouseX, mouseY, 50, height, mouseX, mouseY, 50, height);
   }
 
   function draw() {
-    background(0);
+    background(0, 0, 0, 0);
     fill('#FFFFFF');
     //imageMode(CENTER);
     //image(gif, width/2, height/2)
@@ -17,9 +31,9 @@ function windowResized() {
     text('every living creature yearns to gnaw our bones', width / 2, height * 4/6);
     text('eyes glint in vengeance, urging us to suicide', width / 2, height * 5/6);
     
-    push();
-        fill('#000000');
-        rect(0, height * 3.5/6, mouseX, height/8);
-        rect(0, height * 4.5/6, mouseX - height/7, height/8);
-    pop();
+    //push();
+        //fill('#000000');
+        //rect(0, height * 3.5/6, mouseX, height/8);
+        //rect(0, height * 4.5/6, mouseX - height/7, height/8);
+    //pop();
   }
--- warptext.js
+++ warptext.js
@@ -2,6 +2,40 @@ let font
 let points
 
 
+// this class describes the properties of a single particle.
+class Particle {
+  // setting the co-ordinates, radius and the
+  // speed of a particle in both the co-ordinates axes.
+    constructor(){
+      this.x = random(0, width);
+      this.y = random(1, height);
+      this.r = random(1,3);
+      this.xSpeed = random(-2,2);
+      this.ySpeed = random(-1,1.5);
+    }
+  
+  // creation of a particle.
+    createParticle() {
+      noStroke();
+      fill('#FFFFFF');
+      circle(this.x,this.y,this.r);
+    }
+  
+  // setting the particle in motion.
+    moveParticle() {
+      if(this.x < 0 || this.x > width)
+        this.xSpeed*=-1;
+      if(this.y < 0 || this.y > height)
+        this.ySpeed*=-1;
+      this.x+=this.xSpeed;
+      this.y+=this.ySpeed;
+    }
+  
+  }
+  
+  // an array to add multiple particles
+  let particles = [];
+
 function preload() {
   font = loadFont('Inconsolata-Bold.ttf');
 }
@@ -18,6 +52,10 @@ function setup() {
     sampleFactor: 0.1,
     simplifyThreshold: 0
   })
+
+  for(let i = 0;i<width/2;i++){
+    particles.push(new Particle());
+  }
 
 }
 
@@ -66,4 +104,9 @@ function draw() {
     textFont('Adobe Garamond Pro');
     text('a gaze that admits no ray of light', width * 2/5, height * 4.5/6);
     text('overcome by treachery\'s grief', width * 3/5, height * 5/6);
+
+  for(let i = 0;i<particles.length;i++) {
+      particles[i].createParticle();
+      particles[i].moveParticle();
+    }
 }
