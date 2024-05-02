0bb50f471c593883d11b02b6468ef3241dc9bd72
dgsquared <diego.dgomez@outlook.com>
2024-05-02 00:05:12 -0700

    end it all

--- jitter.js
+++ jitter.js
@@ -1,18 +1,22 @@
-    
-      function windowResized() {
-        resizeCanvas(windowWidth, windowHeight);
-      }
-      
-      function setup() {
-        createCanvas(windowWidth, windowHeight);
-        background(0, 0);
-        
-      }
-      
-      function draw() {
-        fill('#FFFFFF');
-        textAlign(CENTER);
-        textSize(30);
-        textFont('Adobe Garamond Pro');
-        text('our atmosphere refuses to enfold us', width / 2, height * 5/6);
-      }
+
+  
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background ();
+    noStroke();
+    strokeWeight();
+    fill('#FFFFFF');
+    textAlign(CENTER);
+    textSize(50);
+    textFont('Adobe Garamond Pro');
+    text('our atmosphere refuses to enfold us', width / 2, height * 5/6);
+
+
+  }
